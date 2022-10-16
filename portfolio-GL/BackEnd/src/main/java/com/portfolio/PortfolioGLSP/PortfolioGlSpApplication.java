ee914fbfe144c2f20e6b58124951c5e2a6c502b4
Gonzalo Leyes <gonzaleyes97@gmail.com>
2022-10-15 21:53:28 -0300

    Creacion de carpetas FrontEnd y Backend

---
+++ portfolio-GL/BackEnd/src/main/java/com/portfolio/PortfolioGLSP/PortfolioGlSpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioGLSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioGlSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioGlSpApplication.class, args);
+	}
+
+}
---
+++ portfolio-GL/BackEnd/src/test/java/com/portfolio/PortfolioGLSP/PortfolioGlSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioGLSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioGlSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
